8588196139e7cd679f184444faad9edd302427e7
Justin Peisker <justinpeisker@gmail.com>
2022-01-10 14:52:28 -0800

    delete bug fixes

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ server.post('/api/users', async(req, res) => {
             res.status(201).json(newUser)
         }
     } catch (err) {
-        res.status(500).json({ message: err.message})
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
     }
 })
 
@@ -26,7 +26,7 @@ server.get('/api/users', async (req, res) => {
         const users = await User.find()
         res.json(users)
     }catch (err) {
-        res.status(500).json({message: err.message})
+        res.status(500).json({message: "The users information could not be retrieved" })
     }
 })
 
@@ -48,12 +48,12 @@ server.delete('/api/users/:id', async (req, res) => {
     try{
         const deletedUser = User.remove(req.params.id)
         if(!deletedUser){
-            res.status(404).json({message: 'No user with that id'})
+            res.status(404).json({message: "The user with the specified ID does not exist"})
         } else{
-            res.status(200).json(deletedUser)
+            res.json(deletedUser)
         }
     }catch (err) {
-        res.status(500).json({message: err.message})
+        res.status(500).json({message: "The user could not be removed" })
     }
 })
 
