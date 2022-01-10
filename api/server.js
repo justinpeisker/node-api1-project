797f82949171c0bd2eeed76d69fc25e085764c38
Justin Peisker <justinpeisker@gmail.com>
2022-01-10 14:11:34 -0800

    post debugging

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ server.use(express.json())
 server.post('/api/users', async(req, res) => {
     try{
         const {name, bio} = req.body
-        const newUser = await User.create({name, bio})
+        const newUser = await User.insert({name, bio})
         res.status(201).json(newUser)
     } catch (err) {
         res.status(500).json({ message: err.message})
