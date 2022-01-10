e95e7dd7b963223d1ba2c26b44a4b73a705c87e1
Justin Peisker <justinpeisker@gmail.com>
2022-01-10 14:38:53 -0800

    post 400 error fixed

--- api/server.js
+++ api/server.js
@@ -11,7 +11,11 @@ server.post('/api/users', async(req, res) => {
     try{
         const {name, bio} = req.body
         const newUser = await User.insert({name, bio})
-        res.status(400).json(newUser)
+        if(!name && !bio){
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+            res.status(201).json(newUser)
+        }
     } catch (err) {
         res.status(500).json({ message: err.message})
     }
