71b08ff4bdf5180b82a13a4c9e4a72ce494e5250
Justin Peisker <justinpeisker@gmail.com>
2022-01-11 10:08:40 -0800

    bugs fixed with help

--- api/server.js
+++ api/server.js
@@ -7,18 +7,21 @@ const server = express()
 
 server.use(express.json())
 
-server.post('/api/users', async(req, res) => {
-    try{
-        const {name, bio} = req.body
-        const newUser = await User.insert({name, bio})
-        if(!name && bio){
-            res.status(400).json({ message: "Please provide name and bio for the user" })
-        } else {
+server.post('/api/users', (req, res) => {
+
+    const user = req.body
+    if(!user.name || !user.bio){
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+        User.insert(user)
+        .then(newUser => {
             res.status(201).json(newUser)
-        }
-    } catch (err) {
-        res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
+        .catch(err =>{
+            res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
     }
+        
 })
 
 server.get('/api/users', async (req, res) => {
@@ -46,31 +49,32 @@ server.get('/api/users/:id', async (req,res) => {
 })
 
 server.delete('/api/users/:id', async (req, res) => {
-    try{
-        const deletedUser = User.remove(req.params.id)
-        if(!deletedUser){
+        const user = await User.findById(req.params.id)
+        
+        if(!user){
             res.status(404).json({message: "The user with the specified ID does not exist"})
-        } else{
-            res.json(deletedUser)
+        } else {
+           const deletedUser = await User.remove(user.id)
+            res.status(200).json(deletedUser)        
         }
-    }catch (err) {
-        res.status(500).json({message: "The user could not be removed" })
-    }
 })
 
-server.put('/api/users', async (req, res) => {
-    const { id } = req.params
-    const { name, bio } = req.body
+server.put('/api/users/:id', async (req, res) => {
+
+    const user = req.body
     try{
-       const updatedUser = await User.update(id, {name, bio}) 
-       if(!updatedUser){
+       const searchedUser = await User.findById(req.params.id) 
+       if(!searchedUser){
             res.status(404).json({message: "The user with the specified ID does not exist" })
-        } else if(!name && bio){
+        } else {
+            if(!user.name || !user.bio){
             res.status(400).json({message: "Please provide name and bio for the user"})
+        
         }else {
-           res.json(updatedUser)
-       }
-    } catch{
+            const updatedUser = await User.update(req.params.id, req.body)
+           res.status(200).json(updatedUser)
+       }}
+    } catch (err) {
         res.status(500).json({message: "The user information could not be modified" })
     }
 })
